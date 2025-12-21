ea6f9be7d0684d669b27609e629b52b7123e9019
Siddharth Dhaigude <sd8capricon@gmail.com>
2025-12-22 01:15:43 +0530

    base reader

---
+++ ingestion/readers/base.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+class BaseReader(ABC):
+
+    def __init__(self, file_path: Path, chunk_size: int = 512, chunk_overlap: int = 32):
+        super().__init__()
+
+        self.text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=chunk_size, chunk_overlap=chunk_overlap
+        )
+        self.file_path = file_path
+
+    @abstractmethod
+    def _read_file(self):
+        pass
+
+    @abstractmethod
+    def get_documents(self) -> list[Document]:
+        pass
