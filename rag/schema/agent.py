c52c82c12ce8d39dbe7d33e28669f47647cae156
Siddharth Dhaigude <sd8capricon@gmail.com>
2026-01-18 02:59:17 +0530

    llm object in context

--- rag/schema/agent.py
+++ rag/schema/agent.py
@@ -1,8 +1,13 @@
 from dataclasses import dataclass
 
+from langchain_core.language_models.chat_models import BaseChatModel
+from langchain_neo4j.vectorstores.neo4j_vector import Neo4jVector
+
 from rag.schema.retrievers import DriftConfig
 
 
 @dataclass
 class RAGContext:
     drift_config: DriftConfig
+    llm: BaseChatModel
+    vector_store: Neo4jVector
--- rag/tools/search.py
+++ rag/tools/search.py
@@ -1,21 +1,17 @@
 from langchain.tools import ToolRuntime, tool
-from langchain_neo4j.vectorstores.neo4j_vector import Neo4jVector
-from langchain_openai import ChatOpenAI
 
 from rag.retrievers.drift import drift_search
 from rag.schema.agent import RAGContext
 from rag.utils.retrievers import collect_answers
 
-llm: ChatOpenAI = None
-vector_store: Neo4jVector = None
-
 
 @tool(
-    name_or_callable="search_knowledge_base",
     description="Retrieves specific facts related to the user's query. Use this tool when the user asks for information, evidence, or details that require external lookups.",
 )
 def search_knowledge_base(query: str, runtime: ToolRuntime[RAGContext]) -> str:
     drift_config = runtime.context.drift_config
+    llm = runtime.context.llm
+    vector_store = runtime.context.vector_store
     root = drift_search(query, llm, vector_store, drift_config)
     facts = collect_answers(root)
     # clean up whitespaces and ignore empty string
